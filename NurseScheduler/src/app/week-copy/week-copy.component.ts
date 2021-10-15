8d60ecc6dce31dbd31de3a1f3f4cddf44173c612
gjosenhans <grantjosenhans@gmail.com>
2021-10-15 17:23:25 -0600

    Added week of title and button (no functionality). Rearranged header.

---
+++ NurseScheduler/src/app/week-copy/week-copy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-week-copy',
+  templateUrl: './week-copy.component.html',
+  styleUrls: ['./week-copy.component.css']
+})
+export class WeekCopyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
