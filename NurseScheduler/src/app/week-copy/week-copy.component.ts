8a21b207adfc4eecf3ad8e2a69b8a841ab1e8571
gjosenhans <grantjosenhans@gmail.com>
2021-11-18 16:59:08 -0700

    Week of shows selected date.

--- NurseScheduler/src/app/week-copy/week-copy.component.ts
+++ NurseScheduler/src/app/week-copy/week-copy.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-week-copy',
   templateUrl: './week-copy.component.html',
   styleUrls: ['./week-copy.component.css']
 })
+
 export class WeekCopyComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+  @Input() childMessage: any | undefined;
 
   value = null;
   onEnter(value: any) { this.value = value; }
