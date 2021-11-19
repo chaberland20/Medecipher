f6669a9916b65a6277c670263b8097cef21f46d8
Daniel Ayers <danielayers2019@gmail.com>
2021-11-19 08:10:51 -0700

    shift type dropdowns are organized **needs refactoring**

--- NurseScheduler/src/app/nurse-dropdown/nurse-dropdown.component.ts
+++ NurseScheduler/src/app/nurse-dropdown/nurse-dropdown.component.ts
@@ -17,6 +17,7 @@ export class NurseDropdownComponent implements OnInit {
   currentRun: string = "";
   nurseIds: string[] = [];
   shiftTypes: string[] = [];
+  public userArray: NurseSched[] = [];
   
 
   constructor (private http: HttpClient) {
@@ -38,7 +39,7 @@ export class NurseDropdownComponent implements OnInit {
   ngOnInit(): void {
     let scheduleService = new NurseScheduleService(this.http)
     this.nurseIds = scheduleService.getNurseIds()
-    this.shiftTypes = scheduleService.getShiftTypes()
+    this.getShiftTypes()
   }
 
 
@@ -82,4 +83,81 @@ export class NurseDropdownComponent implements OnInit {
     console.log("adding the following to the schedule:", "\nRun_ID - ", this.currentRun, "\nDate - HARDCODED 11/14/20", "\nRN_ID - ", id, "\nShift_Assigned - ", shift)
   }
 
+
+  // returns list of preexisting shifts (no duplicates)
+  getShiftTypes() {
+  // read .csv
+  this.http.get('assets/Nurse_Shifts.csv',
+  {responseType: 'text'})
+  .subscribe(
+    data => {
+      let csvToRowArray = data.split("\n");
+      // import .csv data into array
+      for (let index = 0; index < csvToRowArray.length - 1; index++) {
+        let row = csvToRowArray[index].split(",");
+        this.userArray.push(new NurseSched( row[0], row[1], row[2], row[3].trim()));
+      }
+      // extract and return list of shifts
+      this.userArray.forEach((s) => {
+        // every shift should contain HR; if the shift isn't a duplicate and has HR, add it
+        if(!this.shiftTypes.includes(s.shift_assigned) && s.shift_assigned.search("HR") != -1) this.shiftTypes.push(s.shift_assigned)
+      })
+      this.organizeShifts()
+    },
+    error => {
+        console.log(error);
+    }
+  );   
+  }
+
+  // orders list of shifts by duration and start time
+  organizeShifts() {
+    var data: shiftData[] = [];
+    var sortedShifts: string[] = [];
+    // extract duration and start time
+    this.shiftTypes.forEach((s) => {      
+      var startTime = +s.substring(0, 2)
+      var duration = +s.substring(5, 7) 
+      var shift = new shiftData(startTime, duration)
+      data.push(shift);
+    })
+    // sort by duration, then by start time
+    data = data.sort((a, b) => ( (a.duration < b.duration) || (a.duration == b.duration && a.startTime < b.startTime) ? -1 : 1))
+
+    data.forEach((d) => {
+      sortedShifts.push(d.startTime + ":00 " + d.duration + "hr")
+    })
+
+    this.shiftTypes = sortedShifts;
+  }
+
+
+
+
+}
+
+
+
+export class NurseSched{
+  run_id: string;
+  date: string;
+  rn_id: string;
+  shift_assigned: string;
+
+  constructor(run_id: string, date: string, rn_id: string, shift_assigned: string){
+    this.run_id = run_id;
+    this.date = date;
+    this.rn_id = rn_id
+    this.shift_assigned = shift_assigned;
+  }
+}
+
+export class shiftData{
+  startTime: number;
+  duration: number;
+
+  constructor(startTime: number, duration: number) {
+    this.duration = duration;
+    this.startTime = startTime;
+  }
 }
