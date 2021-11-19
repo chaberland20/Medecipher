9d257c75319470be40dde282f1fb2b9d844c333f
gjosenhans <grantjosenhans@gmail.com>
2021-11-19 12:26:26 -0700

    Week of now shows the previous Monday relative to selected calendar date.

--- NurseScheduler/src/app/week-copy/week-copy.component.ts
+++ NurseScheduler/src/app/week-copy/week-copy.component.ts
@@ -14,7 +14,13 @@ export class WeekCopyComponent implements OnInit {
 
   }
 
-  @Input() childMessage: any | undefined;
+  @Input('childMessage') weekOf: any | undefined; /* date gets passed from calendar to app to sidebar to here */
+
+  /* Takes calendar selected date and finds previous Monday */
+  getMonday() {
+      this.weekOf.setDate(this.weekOf.getDate() - (this.weekOf.getDate() + 6) % 7);
+      return this.weekOf;
+  }
 
   value = null;
   onEnter(value: any) { this.value = value; }
