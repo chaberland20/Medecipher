f5278011be25bae641e870960e2fdcdac990467d
gjosenhans <grantjosenhans@gmail.com>
2021-11-29 18:24:39 -0700

    Changed name of function getMonday() to getSunday() since we changed to display Sunday's date.

--- NurseScheduler/src/app/week-copy/week-copy.component.ts
+++ NurseScheduler/src/app/week-copy/week-copy.component.ts
@@ -17,7 +17,7 @@ export class WeekCopyComponent implements OnInit {
   @Input('childMessage') today: any | undefined; /* date gets passed from calendar to app to sidebar to here */
 
   /* Takes calendar selected date and finds previous Sunday */
-  getMonday() {
+  getSunday() {
     var weekOf: Date = new Date();
     weekOf.setDate(this.today.getDate() - (this.today.getDate() + 7) % 7);
     return weekOf;
